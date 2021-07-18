6397dcde19bc20a6db0d7919099f523fdf7eabc5
YLVN <duongy96@gmail.com>
2021-07-19 02:00:18 +0700

    header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import {UserIcon} from '../svg/UserIcon';
+
+interface IHeaderProps {}
+export const Header: React.FC<IHeaderProps> = (props) => {
+    return (
+        <nav className='flex justify-between bg-neutral h-5.5 items-center px-2 pb-0.5'>
+            <a href='#' className='text-primary font-extrabold text-24'>
+                MovieX
+            </a>
+            <ul className='flex gap-1'>
+                <li>
+                    <a className='text-white font-bold text-12 hover:text-primary cursor-pointer'>
+                        Lịch chiếu
+                    </a>{' '}
+                </li>
+                <li>
+                    <a className='text-white font-bold text-12 hover:text-primary cursor-pointer'>
+                        Cụm rạp
+                    </a>
+                </li>
+                <li>
+                    <a className='text-white font-bold text-12 hover:text-primary cursor-pointer'>
+                        Tin tức
+                    </a>
+                </li>
+                <li>
+                    <a className='text-white font-bold text-12 hover:text-primary cursor-pointer'>
+                        Ứng dụng
+                    </a>
+                </li>
+            </ul>
+            <div className='flex gap-2'>
+                <form>
+                    <input
+                        type='text'
+                        className='h-3 bg-transparent border-2 rounded-3xl font-bold text-11 border-white focus:outline-none px-2 text-white'
+                        placeholder='Search movie'
+                    />
+                </form>
+                <button className='bg-primary rounded-full w-3 h-3 flex items-center justify-center'>
+                    <UserIcon />
+                </button>
+            </div>
+        </nav>
+    );
+};
---
+++ tailwind.js
@@ -0,0 +1,137 @@
+/**
+ * @Note Please reset react app whenever you modify something inside
+ */
+const config = {
+    purge: ['./src/**/*.{js,ts,jsx,tsx}'],
+    darkMode: false,
+    theme: {
+        screens: {
+            phone: '600px',
+            // => @media (min-width: 600px) { ... }
+
+            laptop: '1024px',
+            // => @media (min-width: 1024px) { ... }
+
+            desktop: '1270px',
+            // => @media (min-width: 1270px) { ... }
+        },
+        colors: {
+            transparent: 'transparent',
+            primary: '#C91818',
+            neutral: '#1D1616',
+            grey: '#818181',
+            white: '#FFFFFF',
+            black: '#2F2F2F',
+        },
+        fontSize: {
+            11: '1.1rem',
+            12: '1.2rem',
+            13: '1.3rem',
+            16: '1.6rem',
+            20: '2rem',
+            24: '2.4rem',
+            33: '3.3rem',
+            36: '3.3rem',
+        },
+        /**
+         * 1 means 10px
+         */
+        spacing: {
+            0: '0',
+            0.5: '0.5rem',
+            1: '1rem',
+            1.5: '1.5rem',
+            2: '2rem',
+            2.5: '2.5rem',
+            3: '3rem',
+            3.5: '3.5rem',
+            4: '4rem',
+            4.5: '4.5rem',
+            5: '5rem',
+            5.5: '5.5rem',
+            6: '6rem',
+            7: '7rem',
+            8: '8rem',
+            10: '10rem',
+            11: '11rem',
+            13: '13rem',
+            15: '15rem',
+            20: '20rem',
+            22: '22rem',
+            25: '25rem',
+            30: '30rem',
+            32: '32rem',
+            35: '35rem',
+            40: '40rem',
+            50: '50rem',
+        },
+        opacity: {
+            0: '0',
+            20: '0.2',
+            40: '0.4',
+            60: '0.6',
+            80: '0.8',
+            100: '1',
+        },
+        fontWeight: {
+            hairline: 100,
+            'extra-light': 100,
+            thin: 200,
+            light: 300,
+            normal: 400,
+            medium: 500,
+            semibold: 600,
+            bold: 700,
+            extrabold: 800,
+            'extra-bold': 800,
+            black: 900,
+        },
+        minHeight: {
+            0: '0',
+            5: '5rem',
+            10: '10rem',
+            15: '15rem',
+            20: '20rem',
+            25: '25rem',
+            '1/4': '25%',
+            '1/2': '50%',
+            '3/4': '75%',
+            full: '100%',
+        },
+        minWidth: {
+            0: '0',
+            5: '5rem',
+            10: '10rem',
+            15: '15rem',
+            20: '20rem',
+            25: '25rem',
+            '1/4': '25%',
+            '1/2': '50%',
+            '3/4': '75%',
+            full: '100%',
+        },
+        extend: {
+            gridTemplateColumns: {
+                'auto-1fr': 'auto 1fr',
+                '1fr-auto': '1fr auto',
+            },
+            borderRadius: {
+                md: '5px',
+                lg: '8px',
+                xl: '20px',
+                full: '9999999px',
+            },
+            zIndex: {
+                '-1': '-1',
+            },
+        },
+    },
+    variants: {
+        extend: {
+            borderWidth: ['last'],
+        },
+    },
+    plugins: [],
+};
+
+module.exports = config;
