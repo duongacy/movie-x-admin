1e3cac9eb402e6166696c4d435f02ea4765596ea
Y <Y@Ys-MacBook-Pro.local>
2021-08-11 11:36:19 +0700

    add css

--- src/pages/Home/HomePage.tsx
+++ src/pages/Home/HomePage.tsx
@@ -16,38 +16,101 @@ const HomePage = () => {
             <Trailer />
             <div className="px-1 phone:px-3 py-5 text-s-text film-block">
                 <h6 className="text-20 font-600">Đang chiếu</h6>
-                <div className="mx-auto mt-1.5" style={{width:"150%", marginLeft:"-25%"}}>
+                <div className="mx-auto mt-1.5" style={{ width: '150%', marginLeft: '-25%' }}>
                     <Slider {...settings} className="flex">
-                        <div className="px-0.5">
-                            <img src="images/films/film-1.jpg" className="rounded-xl" alt="" />
+                        <div  className="p-1">
+                            <div
+                                className="aspect-w-16 aspect-h-10 rounded-xl"
+                                style={{
+                                    backgroundImage: "url('images/films/film-1.jpg')",
+                                }}
+                            >
+                                <div className="flex flex-col justify-between py-1 px-2">
+                                    <h6 className="text-s-text text-right font-500">1h45m</h6>
+                                    <h6 className="text-s-text text-16 font-500">
+                                        Sat thu ca voi
+                                    </h6>
+                                </div>
+                            </div>
                         </div>
-                        <div className="px-0.5">
-                            <img src="images/films/film-2.jpg" className="rounded-xl" alt="" />
+                        <div  className="p-1">
+                            <div
+                                className="aspect-w-16 aspect-h-10 rounded-xl "
+                                style={{
+                                    backgroundImage: "url('images/films/film-1.jpg')",
+                                }}
+                            >
+                                <div className="flex flex-col justify-between py-1 px-2">
+                                    <h6 className="text-s-text text-right font-500">1h45m</h6>
+                                    <h6 className="text-s-text text-16 font-500">
+                                        Sat thu ca voi
+                                    </h6>
+                                </div>
+                            </div>
                         </div>
-                        <div className="px-0.5">
-                            <img src="images/films/film-3.jpg" className="rounded-xl" alt="" />
+                        <div  className="p-1">
+                            <div
+                                className="aspect-w-16 aspect-h-10 rounded-xl "
+                                style={{
+                                    backgroundImage: "url('images/films/film-1.jpg')",
+                                }}
+                            >
+                                <div className="flex flex-col justify-between py-1 px-2">
+                                    <h6 className="text-s-text text-right font-500">1h45m</h6>
+                                    <h6 className="text-s-text text-16 font-500">
+                                        Sat thu ca voi
+                                    </h6>
+                                </div>
+                            </div>
                         </div>
-                        <div className="px-0.5">
-                            <img src="images/films/film-2.jpg" className="rounded-xl" alt="" />
+                        <div  className="p-1">
+                            <div
+                                className="aspect-w-16 aspect-h-10 rounded-xl "
+                                style={{
+                                    backgroundImage: "url('images/films/film-1.jpg')",
+                                }}
+                            >
+                                <div className="flex flex-col justify-between py-1 px-2">
+                                    <h6 className="text-s-text text-right font-500">1h45m</h6>
+                                    <h6 className="text-s-text text-16 font-500">
+                                        Sat thu ca voi
+                                    </h6>
+                                </div>
+                            </div>
                         </div>
-                        <div className="px-0.5">
-                            <img src="images/films/film-3.jpg" className="rounded-xl" alt="" />
+                        <div  className="p-1">
+                            <div
+                                className="aspect-w-16 aspect-h-10 rounded-xl "
+                                style={{
+                                    backgroundImage: "url('images/films/film-1.jpg')",
+                                }}
+                            >
+                                <div className="flex flex-col justify-between py-1 px-2">
+                                    <h6 className="text-s-text text-right font-500">1h45m</h6>
+                                    <h6 className="text-s-text text-16 font-500">
+                                        Sat thu ca voi
+                                    </h6>
+                                </div>
+                            </div>
                         </div>
-                        <div className="px-0.5">
-                            <img src="images/films/film-1.jpg" className="rounded-xl" alt="" />
+                        <div  className="p-1">
+                            <div
+                                className="aspect-w-16 aspect-h-10 rounded-xl "
+                                style={{
+                                    backgroundImage: "url('images/films/film-1.jpg')",
+                                }}
+                            >
+                                <div className="flex flex-col justify-between py-1 px-2">
+                                    <h6 className="text-s-text text-right font-500">1h45m</h6>
+                                    <h6 className="text-s-text text-16 font-500">
+                                        Sat thu ca voi
+                                    </h6>
+                                </div>
+                            </div>
                         </div>
-                        <div className="px-0.5">
-                            <img src="images/films/film-1.jpg" className="rounded-xl" alt="" />
-                        </div>
-                        <div className="px-0.5">
-                            <img src="images/films/film-1.jpg" className="rounded-xl" alt="" />
-                        </div>
-                        <div className="px-0.5">
-                            <img src="images/films/film-1.jpg" className="rounded-xl" alt="" />
-                        </div>
-                        <div className="px-0.5">
-                            <img src="images/films/film-1.jpg" className="rounded-xl" alt="" />
-                        </div>
+                        
+
+                        
                     </Slider>
                 </div>
             </div>
